06392e021648f783a6e58f4269c0ea18576c3b47
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-10-27 04:13:34 -0500

    .

--- src/components/StoreSection/StoreSection.jsx
+++ src/components/StoreSection/StoreSection.jsx
@@ -2,23 +2,20 @@ import { ProductCard } from "../ProductCard/ProductCard";
 import styles from "./StoreSection.module.css";
 
 export const StoreSection = ({ productList, sectionTitle }) => {
-
   return (
     <div className={styles.boxStoreSection}>
       <h2 className={styles.sectionTitle}>{sectionTitle}</h2>
       <div className={styles.boxProducts}>
-        {
-          productList.map((product) => (
-            <ProductCard
-              key={product.id}
-              id={product.id}
-              category={product.category}
-              image={product.image}
-              name={product.title}
-              price={product.price}
-            />
-          ))
-        }
+        {productList.map((product) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            category={product.category}
+            image={product.image}
+            name={product.title} // Acá es donde se hace el cambio de title a name
+            price={product.price}
+          />
+        ))}
       </div>
     </div>
   );
